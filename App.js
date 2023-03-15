d256e300a9d4aa00604def763302399183ba4140
schenna217 <90796839+schenna217@users.noreply.github.com>
2023-03-15 12:54:57 -0500

    Created new repository, worked on tab nav

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather, MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
+
+const {Navigator, Screen} = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Navigator 
+          screenOptions={
+            ({ route }) => ({
+              tabBarIcon: ({ focused, }) => {
+                let c = focused ? 'red' : 'green';
+                let rn = route.name
+  
+                if (rn == "ContractHistory")
+                  return <FontAwesome5 name={'file-invoice-dollar'} size={24} color={c} />
+                else if (rn == "Info")
+                  return <FontAwesome5 name={'info-circle'} size={24} color={c} />
+                else if (rn == "Money")
+                  return <FontAwesome5 name={'money-check-alt'} size={24} color={c} />
+                return <FontAwesome5 name={'sign-out-alt'} size={24} color={c} />
+  
+              },
+              tabBarShowLabel: false,
+              headerShown: false,
+            })
+          }
+          initialRouteName={"Home"}
+        >
+          <Screen name="Home" component={<div>hello</div>}/>
+        </Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto"/>
+    </>
   );
 }
 
