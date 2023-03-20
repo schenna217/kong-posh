80e896a30acf3e2146c6bb930dcbb827d4c3a6ad
MiheerDGH <90772705+MiheerDGH@users.noreply.github.com>
2023-03-20 12:54:59 -0500

    worked on tab navigation and created basic screens

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Feather, MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
+import { Feather, Entypo, FontAwesome5 } from '@expo/vector-icons';
 import Home from './screens/Home'
 import SignIn from './screens/SignIn'
 import SignUp from './screens/SignUp'
+import Cart from './screens/Cart'
 
 const {Navigator, Screen} = createBottomTabNavigator();
 
@@ -20,12 +21,10 @@ export default function App() {
                 let c = focused ? 'red' : 'green';
                 let rn = route.name
   
-                if (rn == "ContractHistory")
-                  return <FontAwesome5 name={'file-invoice-dollar'} size={24} color={c} />
-                else if (rn == "Info")
-                  return <FontAwesome5 name={'info-circle'} size={24} color={c} />
-                else if (rn == "Money")
-                  return <FontAwesome5 name={'money-check-alt'} size={24} color={c} />
+                if (rn == "Home")
+                  return <Entypo name="home" size={24} color="black" />
+                if (rn == "Cart")
+                  return <FontAwesome5 name="opencart" size={24} color="black" />
                 return <FontAwesome5 name={'sign-out-alt'} size={24} color={c} />
   
               },
@@ -36,6 +35,8 @@ export default function App() {
           initialRouteName={"Home"}
         >
           <Screen name="Home" component={Home}/>
+          <Screen name="SignIn" component={SignIn}/>
+          <Screen name="Cart" component={Cart}/>
         </Navigator>
       </NavigationContainer>
       <StatusBar style="auto"/>
