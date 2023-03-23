a98c0b1ab9ee10201a94829e742d49dfda5e3b37
MiheerDGH <90772705+MiheerDGH@users.noreply.github.com>
2023-03-23 12:55:45 -0500

    worked on UI and creating Scrollable Listview for Cart screen

--- screens/Cart.js
+++ screens/Cart.js
@@ -1,13 +1,43 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import Button from '../components/Button'
+import Item from './Item'
 
 const Cart = () => {
+
+    const names = [
+        {name: 'Item 1', key: 1},
+        {name: 'Item 2', key: 2},
+        {name: 'Item 3', key: 3},
+        {name: 'Item 4', key: 4},
+        {name: 'Item 5', key: 5},
+        {name: 'Item 6', key: 6},
+        {name: 'Item 7', key: 7},
+        {name: 'Item 8', key: 8}
+    
+    ];
+
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>
-                Welcome To Cart
+            <Text style={styles.headerText}>
+                Welcome To Cart!
             </Text>
-
+            <Text style={styles.text}>
+                Total amount:
+            </Text>
+            <Text style={styles.text2}>
+                $$$$$$$
+            </Text>
+            <ScrollView>
+                {names.map( name => {
+                    return(
+                        <View key={ name.key } >
+                            <Text style={styles.listText}>{name.name}</Text>
+                        </View>
+                    );
+                })}
+            </ScrollView>
         </View>
     )
 }
@@ -16,13 +46,33 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
       },
+    headerText:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginLeft: 75,
+        marginTop: 30,
+    },
+    listText:{
+        fontSize: 16,
+        color: 'dodgerblue',
+        fontWeight: 'bold',
+    },
     text:{
         fontSize: 16,
         color: 'dodgerblue',
         fontWeight: 'bold',
+        marginLeft: 10,
+        marginTop: 40,
+    },
+    text2:{
+        alignItems: 'baseline',
+        fontSize: 20,
+        color: 'red',
+        fontWeight: 'bold',
+        marginLeft: 10,
+        
     }
 })
 
