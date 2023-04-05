2fb9dfd4f37d1b93402cfce5fc66963e47fef2f5
veer277 <90796821+veer277@users.noreply.github.com>
2023-04-05 12:52:08 -0500

    Connected code to firebase project

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4UoWX6VnnhPxPQgf4fgrzKhD00WaT8Gc",
+  authDomain: "kong-posh.firebaseapp.com",
+  projectId: "kong-posh",
+  storageBucket: "kong-posh.appspot.com",
+  messagingSenderId: "209742470577",
+  appId: "1:209742470577:web:7358a50e239fabc700f190"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
