b041259225cee634388b18beec45e1cad1694c93
veer277 <90796821+veer277@users.noreply.github.com>
2023-04-06 19:39:57 -0500

    Had to move component to separate file and fixed some merge conflicts

---
+++ components/ProfileIcon.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Avatar } from 'react-native-paper';
+
+const ProfileIcon = () => (
+    <Avatar.Text size={36} label="KP" />
+  );
+  export default ProfileIcon
--- components/ProfileTextAvatar.js
+++ components/ProfileTextAvatar.js
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import { Avatar } from 'react-native-paper';
 
-<<<<<<< Updated upstream
+
 const ProfileTextAvatar = () => (
   <Avatar.Text size={24} label="XD" />
 );
 
 export default ProfileTextAvatar;
-=======
-const ProfileIcon = () => (
-  <Avatar.Text size={36} label="KP" />
-);
-export default ProfileIcon
->>>>>>> Stashed changes
+
+
+
