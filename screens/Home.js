de0b5a5d58898b89954e4ae3a3bdc26c729fe119
sawan18 <sawan.pandita18@gmail.com>
2023-04-08 23:59:23 -0500

    Final additons

--- screens/Home.js
+++ screens/Home.js
@@ -3,18 +3,20 @@ import {StyleSheet, View, SafeAreaView} from 'react-native'
 import Profile from '../components/ProfileTextAvatar'
 import Menu from "./Menu";
 import Button from "../components/Button";
+import Categories from "../components/Categories";
 
 
 export default function Home({navigation}) {
   return (
-    //<SafeAreaView>
-    <View style={styles.container}>
-          <Button
-            title = 'Menu'
-            onPress={() => navigation.navigate(Menu)}
-          />
-    </View>
-    //</SafeAreaView>
+    <SafeAreaView>
+        <View style={styles.container}>
+              <Button
+                title = 'Menu'
+                onPress={() => navigation.navigate(Menu)}
+              />
+        </View>
+        <Categories/>
+    </SafeAreaView>
   );
 }
 
