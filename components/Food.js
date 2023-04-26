553dc586188129dca9eb9dea1d1f15b6a8f0dec6
sawan18 <sawan.pandita18@gmail.com>
2023-04-26 09:08:12 -0500

    Uploded all the original images from the catering service and added 7 items price and description.
    Now I just have to add google images to fill the gap of the photos which we can replace. Now I just need to work on uploding the reast of the images and work on some of the UI things.

--- components/Food.js
+++ components/Food.js
@@ -1,31 +1,52 @@
 const Food =[
 {
     id: '1',
-    name: 'Goat Curry',
-    description: 'Tastes Like goat curry',
-    price: '$25.50',
-    image: require('../assets/images/bread.png')
+    name: 'Channa Sabzi',
+    description: 'Channa Sabzi, also known as Chana Masala or Chole, is a popular vegetarian dish originating from the Indian subcontinent. It is made using chickpeas, or channa, that are cooked in a flavorful tomato-based gravy. The dish is known for its bold and robust flavors, with a blend of spices such as cumin, coriander, and garam masala that give it a distinct taste profile. The chickpeas are typically cooked until tender, then simmered in the gravy, often made with onions, tomatoes, and a variety of spices. Channa Sabzi is often garnished with fresh cilantro and served with naan bread, roti, or rice. It is loved for its hearty and satisfying nature, making it a popular option for both everyday meals and special occasions. The dish is also a staple in many vegetarian and vegan diets due to its high protein and fiber content, making it a nutritious and flavorful choice for plant-based eaters.',
+    price: '$6.99',
+    image: require('../assets/images/Channa_Sabzi.JPG')
 },
 {
     id: '2',
-    name: 'Mutton Curry',
-    description: 'Tastes Like mutton curry',
-    price: '$26.50',
-    image: require('../assets/images/coffee.png')
+    name: 'Chicken Changezi',
+    description: 'Chicken Changezi is a popular North Indian dish known for its rich and flavorful preparation. It is believed to have originated in the Mughlai cuisine of India and is named after the famous Mongolian warrior, Genghis Khan, also known as Changez Khan. Chicken Changezi typically features succulent pieces of chicken that are cooked in a luscious and aromatic gravy. The dish is known for its distinct flavors, achieved by using a blend of spices such as cinnamon, cardamom, cloves, and cumin, which give it a warm and fragrant profile. The chicken is often marinated in a yogurt-based marinade with spices and then cooked slowly in the gravy, allowing the flavors to meld together. The gravy of Chicken Changezi is often rich and creamy, with the use of ingredients such as tomatoes, onions, cashews, and cream, which lend it a luxurious texture. The dish is typically garnished with fried onions, fresh cilantro, and sometimes nuts, adding a crunchy texture and enhancing the presentation.',
+    price: '$18.99',
+    image: require('../assets/images/Chicken_Changezi.JPG')
 },
 {
     id: '3',
-    name: 'Chicken',
-    description: 'Tastes Like chicken biryani',
-    price: '$27.50',
-    image: require('../assets/images/fast-food.png')
+    name: 'Dum Aloo',
+    description: 'Kashmiri Dum Aloo is a popular vegetarian dish from the Kashmiri cuisine of India. It is made using baby potatoes that are cooked in a rich and flavorful gravy. The dish is known for its unique blend of spices, including Kashmiri red chili powder, fennel seeds, ginger, and cinnamon, which lend a distinct flavor profile. The potatoes are first deep-fried until golden and crispy, then simmered in a thick tomato-based gravy, often infused with yogurt or cream to give it a creamy texture. The dish is typically slow-cooked on low heat or "dum" style, allowing the flavors to meld together and create a deliciously aromatic dish. Kashmiri Dum Aloo is often garnished with fresh cilantro or chopped nuts, adding a burst of color and crunch to the dish. It is typically served with naan bread or rice, and is loved for its rich and satisfying taste that is perfect for special occasions or gatherings.',
+    price: '$15.99',
+    image: require('../assets/images/Dum_Aloo.JPG')
 },
 {
     id: '4',
     name: 'Dum Biryani',
-    description: 'Tastes Like dum biryani',
+    description: 'Chicken Biryani is a popular and aromatic rice dish that originated in the Indian subcontinent. It typically consists of succulent pieces of chicken marinated in a blend of spices, layered with fragrant basmati rice, and cooked to perfection. The dish is known for its rich flavors and complex aroma, created by the use of spices such as cardamom, cinnamon, cloves, and saffron. Chicken Biryani is often garnished with fried onions, fresh cilantro, and mint leaves, adding freshness to the dish. It is typically served with yogurt raita or a side of spicy curry, and is loved by food enthusiasts for its delicious taste and unique combination of flavors.',
     price: '$28.50',
-    image: require('../assets/images/soft-drink.png')
+    image: require('../assets/images/Dum_Biryani.JPG')
+},
+{
+    id: '5',
+    name: 'Flat Roth',
+    description: 'Kashmiri Flat Roth, also known as Kashmiri Roth, is a traditional sweet bread from the Kashmiri cuisine, popular in India’s Kashmir valley. It is a small, round, flat bread that is usually made with all-purpose flour, clarified butter (ghee), and sugar, and flavored with cardamom, saffron, and sometimes, fennel seeds.The dough for Kashmiri Flat Roth is prepared by combining the flour, ghee, and sugar, and then flavored with cardamom and saffron. The dough is then divided into small balls, which are flattened into discs and usually decorated with a pattern using a fork or the back of a spoon. The discs are then fried in ghee or oil until they turn golden brown and crispy. The fried bread is soaked in a sugar syrup flavored with cardamom and saffron, giving it a sweet and aromatic taste.',
+    price: '$9.99',
+    image: require('../assets/images/Flat_Roth.JPG')
+},
+{
+    id: '6',
+    name: 'Gobi & Paneer Ki Sabzi',
+    description: 'Gobi and Paneer ki Sabzi is a delicious and wholesome Indian dish made with cauliflower (gobi) and paneer (cottage cheese) cooked together in a spiced tomato-based gravy. The cauliflower and paneer are cooked until tender and coated in a flavorful masala made with a blend of spices like cumin, coriander, turmeric, and red chili powder. The dish is typically finished with a garnish of fresh coriander leaves and served hot with roti (Indian flatbread) or rice. Its a popular vegetarian dish that is loved for its rich and creamy flavors, making it a comforting and satisfying option for lunch or dinner.',
+    price: '$10.99',
+    image: require('../assets/images/Gobi_Paneer_Ki_Sabzi.JPG')
+},
+{
+    id: '7',
+    name: 'Kashmiri Laddu',
+    description: 'Kashmiri Laddu is a traditional sweet delicacy from the Kashmiri cuisine, which originates from the Kashmir Valley in India. It is a popular dessert made from a combination of ingredients including roasted cashews, ghee (clarified butter), sugar, cardamom, saffron, and sometimes rose water. The roasted cashews are ground to a fine powder and then mixed with ghee, sugar, and aromatic spices to form a dough-like consistency. The mixture is then shaped into small balls or discs and garnished with saffron strands or chopped nuts for added flavor and visual appeal. Kashmiri Laddu is known for its rich, nutty flavor, delicate fragrance from the cardamom and saffron, and its melt-in-your-mouth texture. It is often served during festive occasions, weddings, and other celebrations as a symbol of joy and happiness.',
+    price: '$5.99',
+    image: require('../assets/images/Kashmiri_Laddu.JPG')
 },
 ];
 
