03473ffac9395dd4bc849def47a119806eb776ee
veer277 <90796821+veer277@users.noreply.github.com>
2023-05-10 12:45:27 -0500

    Added items to individual arrays

--- config/Snacks.js
+++ config/Snacks.js
@@ -1,12 +1,26 @@
 const Snacks = [
     {
-    id: '1',
-    name: 'Channa Sabzi',
-    description: 'Channa Sabzi, also known as Chana Masala or Chole, is a popular vegetarian dish originating from the Indian subcontinent. It is made using chickpeas, or channa, that are cooked in a flavorful tomato-based gravy. The dish is known for its bold and robust flavors, with a blend of spices such as cumin, coriander, and garam masala that give it a distinct taste profile. The chickpeas are typically cooked until tender, then simmered in the gravy, often made with onions, tomatoes, and a variety of spices. Channa Sabzi is often garnished with fresh cilantro and served with naan bread, roti, or rice.',
-    price: '$6.99',
-    quantity: 0,
-    image: require('../assets/images/Channa_Sabzi.png')
-}
+        id: '1',
+        name: 'Channa Sabzi',
+        description: 'Channa Sabzi, also known as Chana Masala or Chole, is a popular vegetarian dish originating from the Indian subcontinent. It is made using chickpeas, or channa, that are cooked in a flavorful tomato-based gravy. The dish is known for its bold and robust flavors, with a blend of spices such as cumin, coriander, and garam masala that give it a distinct taste profile. The chickpeas are typically cooked until tender, then simmered in the gravy, often made with onions, tomatoes, and a variety of spices. Channa Sabzi is often garnished with fresh cilantro and served with naan bread, roti, or rice.',
+        price: '$6.99',
+        quantity: 0,
+        image: require('../assets/images/Channa_Sabzi.png')
+    }, {
+        id: '2',
+        name: 'Flat Roth',
+        description: 'Kashmiri Flat Roth, also known as Kashmiri Roth, is a traditional sweet bread from the Kashmiri cuisine, popular in India’s Kashmir valley. It is a small, round, flat bread that is usually made with all-purpose flour, clarified butter (ghee), and sugar, and flavored with cardamom, saffron, and sometimes, fennel seeds. The fried bread is soaked in a sugar syrup flavored with cardamom and saffron, giving it a sweet and aromatic taste.',
+        price: '$9.99',
+        quantity: 0,
+        image: require('../assets/images/Flat_Roth.png')
+    }, {
+        id: '3',
+        name: 'Kashmiri Roth',
+        description: 'Indulge in the sweet aroma and rich flavors of Kashmir with our Kashmiri Roth, a traditional sweet bread that is a true culinary masterpiece. Made with love and care, our Roth is a soft, flaky, and melt-in-your-mouth delight that will satisfy your sweet tooth like no other. This authentic Kashmiri specialty is prepared with a blend of aromatic spices, nuts, and dried fruits, creating a perfect harmony of flavors. Our Roth is handcrafted with the finest ingredients and baked to perfection, resulting in a golden, crispy crust that gives way to a luscious, sweet filling that will leave you craving for more.',
+        price: '$10.99',
+        quantity: 0,
+        image: require('../assets/images/Kashmiri_Ther_Rice.png')
+    }
 ];
 
 export default Snacks;
